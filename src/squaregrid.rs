fd7ff7b6b76333698c66cce1fcf87a89a7a4ef85
enerqi <enerqi@nodomain.is>
2016-01-13 16:28:53 +0000

    Tidy up SquareGrid impl.

--- src/squaregrid.rs
+++ src/squaregrid.rs
@@ -2,15 +2,16 @@ use petgraph::{Graph, Undirected};
 use rand;
 use rand::distributions::{IndependentSample, Range};
 
+
+pub type GridIndexType = u16;
+pub type GridGraphNodeIndex = ::petgraph::graph::NodeIndex<GridIndexType>;
+
 enum GridDirection {
     North,
     South,
     East,
     West,
 }
-
-type GridIndexType = u16;
-type GridGraphNodeIndex = ::petgraph::graph::NodeIndex<GridIndexType>;
 
 struct AdjacentCells {
     north: Option<GridGraphNodeIndex>,
@@ -37,18 +38,19 @@ pub struct GridCoordinate {
 
 pub struct SquareGrid {
     /// For mapping a 2D gridcoordinate (converted to a 1D index) to a graph node index.
-    graph_node_indices: Vec<GridGraphNodeIndex>,
+    grid_coordinate_to_node_index_lookup: Vec<GridGraphNodeIndex>,
     graph: Graph<AdjacentCells, (), Undirected, GridIndexType>,
 }
 
 impl SquareGrid {
-    fn new(cells_count: GridIndexType) -> Result<SquareGrid, String> {
-        let dim_size: GridIndexType = try!(dimension_size(cells_count));
+    pub fn new(dimension_size: GridIndexType) -> SquareGrid {
+
+        let cells_count = dimension_size * dimension_size;
         let nodes_count_hint = cells_count as usize;
-        let edges_count_hint = 4 * cells_count as usize - 4 * dim_size as usize; // Probably overkill, but don't want any capacity panics
+        let edges_count_hint = 4 * cells_count as usize - 4 * dimension_size as usize; // Probably overkill, but don't want any capacity panics
 
         let mut grid = SquareGrid {
-            graph_node_indices: Vec::with_capacity(nodes_count_hint),
+            grid_coordinate_to_node_index_lookup: Vec::with_capacity(nodes_count_hint),
             graph: Graph::with_capacity(nodes_count_hint, edges_count_hint),
         };
 
@@ -57,24 +59,20 @@ impl SquareGrid {
         // by adding a node to the graph.
         for _ in 0..cells_count {
             let node_index = grid.graph.add_node(AdjacentCells::new());
-            grid.graph_node_indices.push(node_index);
+            grid.grid_coordinate_to_node_index_lookup.push(node_index);
         }
 
-        for (index, node_index) in grid.graph_node_indices.iter().enumerate() {
-            let y = index / dim_size as usize;
-            let x = (y * dim_size as usize) - index;
-            let coord = GridCoordinate {
-                x: x as isize,
-                y: y as isize,
-            };
+        for (index, node_index) in grid.grid_coordinate_to_node_index_lookup.iter().enumerate() {
+
+            let coord = to_grid_coordinate(index, dimension_size);
 
             // limit the lifetime of the find_neighbour_nodeindex closure so we don't get grid borrow conflict
             let (north_index, south_index, east_index, west_index) = {
                 let find_neighbour_nodeindex = |dir: GridDirection| -> Option<GridGraphNodeIndex> {
                     let neighbour_coord = offset_coordinate(&coord, dir);
-                    if is_valid_coordinate(&neighbour_coord, dim_size) {
-                        let node_indices_index = to_1D_index(&neighbour_coord, dim_size);
-                        let node_index = grid.graph_node_indices[node_indices_index as usize];
+                    if is_valid_coordinate(&neighbour_coord, dimension_size) {
+                        let node_indices_index = to_1d_index(&neighbour_coord, dimension_size);
+                        let node_index = grid.grid_coordinate_to_node_index_lookup[node_indices_index as usize];
                         Some(node_index)
                     } else {
                         None
@@ -86,26 +84,30 @@ impl SquareGrid {
                  find_neighbour_nodeindex(GridDirection::West))
             };
 
-            // let this_node_data: &mut AdjacentCells = grid.graph
-            //                                              .node_weight_mut(node_index.clone())
-            //                                              .unwrap();
+            let adjacent_cells = grid.graph
+                                     .node_weight_mut(node_index.clone())
+                                     .unwrap();
+            adjacent_cells.north = north_index;
+            adjacent_cells.south = south_index;
+            adjacent_cells.east = east_index;
+            adjacent_cells.west = west_index;
         }
 
-        Ok(grid)
+        grid
     }
 
-    fn random_cell(&self) -> GridGraphNodeIndex {
-        let random_pos = Range::new(0, self.graph_node_indices.len());
+    pub fn random_cell(&self) -> GridGraphNodeIndex {
+        let random_pos = Range::new(0, self.grid_coordinate_to_node_index_lookup.len());
         let mut rng = rand::thread_rng();
         let index = random_pos.ind_sample(&mut rng);
-        self.graph_node_indices[index]
+        self.grid_coordinate_to_node_index_lookup[index]
     }
 
-    fn link(&mut self, a: GridGraphNodeIndex, b: GridGraphNodeIndex) {
+    pub fn link(&mut self, a: GridGraphNodeIndex, b: GridGraphNodeIndex) {
         let _ = self.graph.update_edge(a, b, ());
     }
 
-    fn unlink(&mut self, a: GridGraphNodeIndex, b: GridGraphNodeIndex) {
+    pub fn unlink(&mut self, a: GridGraphNodeIndex, b: GridGraphNodeIndex) {
 
         if let Some(edge_index) = self.graph.find_edge(a, b) {
             // This will invalidate the last edge index in the graph, which is fine as we
@@ -115,16 +117,16 @@ impl SquareGrid {
     }
 
     /// Cell nodes that are linked to a particular node by a passage.
-    fn links(&self, node_index: GridGraphNodeIndex) -> Vec<GridGraphNodeIndex> {
+    pub fn links(&self, node_index: GridGraphNodeIndex) -> Vec<GridGraphNodeIndex> {
         self.graph
             .edges(node_index)
-            .map(|index_edge_data_pair| {index_edge_data_pair.0.clone()})
+            .map(|index_edge_data_pair| index_edge_data_pair.0.clone())
             .collect()
     }
 
     /// Cell nodes that are to the North, South, East or West of a particular node, but not
     /// necessarily linked by a passage.
-    fn neighbours(&self, node_index: GridGraphNodeIndex) -> Vec<GridGraphNodeIndex> {
+    pub fn neighbours(&self, node_index: GridGraphNodeIndex) -> Vec<GridGraphNodeIndex> {
 
         if let Some(adj) = self.graph.node_weight(node_index) {
             vec![adj.north, adj.south, adj.east, adj.west]
@@ -138,18 +140,6 @@ impl SquareGrid {
     }
 }
 
-fn dimension_size(grid_cells_count: GridIndexType) -> Result<GridIndexType, String> {
-
-    let dim_size = (grid_cells_count as f64).sqrt().trunc() as GridIndexType;
-    if dim_size * dim_size == grid_cells_count {
-        Ok(dim_size)
-    } else {
-        Err(format!("For a square grid the integer square root of the grid_cells_count {} should \
-                     equal grid_cells_count.",
-                    grid_cells_count))
-    }
-}
-
 fn offset_coordinate(coord: &GridCoordinate, dir: GridDirection) -> GridCoordinate {
     let (x, y) = (coord.x, coord.y);
     match dir {
@@ -160,19 +150,26 @@ fn offset_coordinate(coord: &GridCoordinate, dir: GridDirection) -> GridCoordina
     }
 }
 
-fn is_valid_coordinate(coord: &GridCoordinate, dim_size: GridIndexType) -> bool {
+fn is_valid_coordinate(coord: &GridCoordinate, dimension_size: GridIndexType) -> bool {
     if coord.x < 0 || coord.y < 0 {
         return false;
     }
     let (x, y) = (coord.x, coord.y);
-    if x >= dim_size as isize || y >= dim_size as isize {
+    if x >= dimension_size as isize || y >= dimension_size as isize {
         return false;
     }
     true
 }
 
-#[allow(non_snake_case)]
-fn to_1D_index(coord: &GridCoordinate, dim_size: GridIndexType) -> GridIndexType {
-    ((coord.y * dim_size as isize) + coord.x) as GridIndexType
+fn to_1d_index(coord: &GridCoordinate, dimension_size: GridIndexType) -> GridIndexType {
+    ((coord.y * dimension_size as isize) + coord.x) as GridIndexType
 }
 
+fn to_grid_coordinate(one_dimensional_index: usize, dimension_size: GridIndexType) -> GridCoordinate {
+    let y = one_dimensional_index / dimension_size as usize;
+    let x = (y * dimension_size as usize) - one_dimensional_index;
+    GridCoordinate {
+        x: x as isize,
+        y: y as isize,
+    }
+}
