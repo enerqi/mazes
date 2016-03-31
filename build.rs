d89861d91026b5b1ddee89871d13359a16fb86d9
enerqi <enerqi@enerqi>
2016-03-31 22:57:04 +0100

    Build comments.

--- build.rs
+++ build.rs
@@ -2,20 +2,23 @@ use std::env;
 
 fn main() {
 
+    // Assume libsdl2*-dev etc. are installed with the package manager on unix family systems.
+    // On a windows OS we look for the C built sdl2 libraries in ./sdl_libs.
+    // The `cargo run` command will also look for sdl2 dlls in the same directory.
     if cfg!(target_family = "windows") {
 
         let libs_dir =
-        if let Ok(dir) = env::var("SDL_LIBS_DIR") {
-            Some(dir)
-        }
-        else {
-            if let Ok(cargo_root_dir) = env::var("CARGO_MANIFEST_DIR") {
-                Some(format!("{}/sdl_libs", cargo_root_dir))
+            if let Ok(dir) = env::var("SDL_LIBS_DIR") {
+                Some(dir)
             }
             else {
-                None
-            }
-        };
+                if let Ok(cargo_root_dir) = env::var("CARGO_MANIFEST_DIR") {
+                    Some(format!("{}/sdl_libs", cargo_root_dir))
+                }
+                else {
+                    None
+                }
+            };
 
         if let Some(libs) = libs_dir {
             println!("cargo:rustc-flags=-L {}", libs);
