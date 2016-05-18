349f5011b699a8608962ca8761ee8fa4f989693c
enerqi <enerqi@enerqi>
2016-05-18 22:59:33 +0100

    Renderers bench update.

--- benches/renderers.rs
+++ benches/renderers.rs
@@ -8,19 +8,10 @@ use mazes::renderers;
 use mazes::pathing;
 use mazes::squaregrid;
 
-use std::ptr;
 use test::Bencher;
-
-extern "C" {
-                                            // maybe null function pointer
-    pub fn SDL_LogSetOutputFunction(callback: Option<extern fn()>,
-                                    userdata: Option<extern fn()>);
-}
 
 #[bench]
 fn render_grid(b: &mut Bencher) {
-
-    unsafe { SDL_LogSetOutputFunction(None, None); }
 
     let grid_size = 200;
     let mut maze_grid = squaregrid::SquareGrid::<u32>::new(grid_size);
@@ -37,28 +28,18 @@ fn render_grid(b: &mut Bencher) {
              .distances(distances.as_ref())
              .build();
 
-    // Todo - why does SDL_LogCritical get called so much by fill_rect/draw_line?
+    // Why does SDL_LogCritical get called so much by fill_rect/draw_line? At least according to the CodeXL sampling profiler.
     // render recursive-backtracker image --mark-start-end --grid-size 2500 --cell-pixels 8 --show-path --image-out=perf-test-maze.png --colour-distances
-    // 23% of samples
-    // and it does not improve when setting SDL_LogSetOutputFunction to nothing
+    // 23% of samples and it does not improve when setting SDL_LogSetOutputFunction to nothing:
+    // extern "C" {
+    //                                         // maybe null function pointer
+    // pub fn SDL_LogSetOutputFunction(callback: Option<extern fn()>,
+    //                                 userdata: Option<extern fn()>);
+    // }
+    // unsafe { SDL_LogSetOutputFunction(None, None); }
+    // Probably a non-issue:
+    // It happens with msvc aswell.
+    // It disappears when using a rebuilt from scratch 64bit msvc SDL lib/dll.
+    // The runtime is basically the same
     b.iter(|| renderers::render_square_grid(&maze_grid, &render_options));
 }
-
-// void SDL_LogSetOutputFunction(SDL_LogOutputFunction callback, void *userdata);
-// static void SDL_LogOutput(void *userdata,
-//                           int category, SDL_LogPriority priority,
-//                           const char *message);
-
-// ?
-// static void
-// debug_print(const char *fmt, ...)
-// {
-//     va_list ap;
-//     va_start(ap, fmt);
-//     SDL_LogMessageV(SDL_LOG_CATEGORY_ASSERT, SDL_LOG_PRIORITY_WARN, fmt, ap);
-//     va_end(ap);
-// }
-//
-// SDL_SetError if in debug
-//
-//  #if LOG_WINDOW_EVENTS==1
